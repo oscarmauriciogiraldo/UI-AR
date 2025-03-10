14850228d919ba6b217b963c819e3908e22713b0
OscarGiraldo <oscar.giraldo@linktic.com>
2025-03-10 00:01:06 -0500

    feat: mocks get url params

--- main.js
+++ main.js
@@ -1,21 +1,25 @@
 window.onload = async () => {
     let testEntityAdded = false;
 
-    /* ####### mock parametros recibidos URL ######## */
     /* funcion para obtener parametros desde la URL */
-    const urlParams = new URLSearchParams(window.location.search)
-    console.log('1. Datos del url params: ', urlParams)
+    function getUrlParams(){
+        const params = new URLSearchParams(window.location.search)
+        return {
+            user: params.get("usr"),
+            userId: params.get("uuid") || "1234",
+            latitude: parseFloat(params.get("lat")) || 4.8029365, // Coordenada mockeada ubicacion casa
+            longitude: parseFloat(params.get("lng")) || -75.7342656, // Coordenada mockeada ubicacion casa
+        }
+
+    }
     /* Prueba mock url */
     /* http://localhost:5173/?lat=4.8029365/&lng=-75.7342656/&usr=dzWnzQ4fkQnVPJj2UfEt/&uuid=2ece92d1a7e54dd3b5a2dc2620afd6af */
     /* Prueba phone: https://6qt78s9s-5173.use2.devtunnels.ms/?lat=4.8029365/&lng=-75.7342656/&usr=dzWnzQ4fkQnVPJj2UfEt/&uuid=2ece92d1a7e54dd3b5a2dc2620afd6af */
     /* Api Real: https://itssoluciones.co/tesoro/?lat=4.8029365/&lng=-75.7342656/&usr=dzWnzQ4fkQnVPJj2UfEt/&uuid=2ece92d1a7e54dd3b5a2dc2620afd6af */
 
-    const user = urlParams.get("usr");
-    const userId = urlParams.get("uuid") || "1234";
-    const latitude = parseFloat(urlParams.get("lat")) || 4.8029365; // Coordenada mockeada ubicacion casa
-    const longitude = parseFloat(urlParams.get("lng")) || -75.7342656; // Coordenada mockeada ubicacion casa
-
-    console.log(`2. Datos recibidos del api mockeada:  Usuario: ${user}, User-ID: ${userId}, Latitud: ${latitude}, Longitud: ${longitude}`);
+    //obtener parámetros (mockeados o reales)
+    const { user, userId, latitude, longitude } = getUrlParams()
+    console.log(`1. Datos recibidos del api mockeada:  Usuario: ${user}, User-ID: ${userId}, Latitud: ${latitude}, Longitud: ${longitude}`);
     /* ####### mock parametros recibidos URL ######## */
     
 
@@ -24,7 +28,7 @@ window.onload = async () => {
     el.addEventListener("gps-camera-update-position", e => {
         if(!testEntityAdded) {
             //alert(`Got first GPS position: lon ${e.detail.position.longitude} lat ${e.detail.position.latitude}`);
-            alert(`3. Ubicacion recibida por parametros: lon ${longitude} lat ${latitude} for user ${user}`);
+            alert(`2. Ubicacion recibida por parametros: lon ${longitude} lat ${latitude} for user ${user}`);
            
             /* Add a model to the nort of the initial GPS position */
             const cofre = document.createElement('a-entity')
@@ -47,12 +51,10 @@ window.onload = async () => {
                 /* Calcula la ubicacion y pone el modelo en estas coordenadas */
                 /* latitude: e.detail.position.latitude + 0.001,
                 longitude: e.detail.position.longitude */
-                /* latitude: data.latitude + 0.001,
-                longitude: data.longitude */
                 latitude: latitude + 0.001,
                 longitude: longitude,
             });
-            console.log('Latitud y longitud recibidas', latitude, longitude)
+            console.log('3. Modelo ubicado en  Latitud y longitud recibidas', latitude, longitude)
             document.querySelector("a-scene").appendChild(cofre);
 
             /*  ***** segundo modelo ******* */
